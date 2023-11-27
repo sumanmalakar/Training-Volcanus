51ea24220dcd4b88f158195e3a5326e80566be72
sumanmalakar <malakars041@gmail.com>
2023-11-27 17:09:21 +0530

    starting with react.js

---
+++ day5_React/volcanus_react/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  const name = "ram";
+  const name2 = "mohan";
+
+  return (
+    <>
+      <div>
+        <h1>My Name is {name}</h1>
+        <p>Your Name is {name2}</p>
+      </div>
+
+      <div>
+        <h1>This is my div </h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
