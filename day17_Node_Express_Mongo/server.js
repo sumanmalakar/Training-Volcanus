7bedc4b0a9e7fb7d27954d9f95e1410093b048a7
sumanmalakar <malakars041@gmail.com>
2023-12-15 16:56:56 +0530

    user registered successfully

--- day17_Node_Express_Mongo/server.js
+++ day17_Node_Express_Mongo/server.js
@@ -1,14 +1,19 @@
-import express from 'express';
-import bodyParser from 'express'
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-mongoose.connect(
-  "mongodb+srv://sumanmalakar2022:G7VwOsxTULrhE9SR@volcanus.tkilonf.mongodb.net/"
-).then(()=>console.log("MongoDB Connected..!"));
+mongoose
+  .connect(
+    "mongodb+srv://sumanmalakar2022:G7VwOsxTULrhE9SR@volcanus.tkilonf.mongodb.net/",
+    {
+      dbName: "Volcanus_MERN",
+    }
+  )
+  .then(() => console.log("MongoDB Connected..!"));
 
 const arr = [];
 
@@ -18,33 +23,67 @@ const arr = [];
 // U = Update -> PUT Method
 // D = Delete -> DELETE Method
 
-// Read 
-app.get('/',(req,res)=>{
-    console.log("This is Home Route")
-    res.send({
-      marvel:'Doctor Strange',
-      Ram:'Maryaada Puroshotam',
-      data:arr
-    });
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  gmail: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
 
-    // res.send("<h1>This is html data</h1>")
-})
+const User = mongoose.model("User", userSchema);
 
-app.get('/superman',(req,res)=>{
-    // console.log("this is superman route")
-    res.send({
-        DC:"Sperman is belong from DC"
-    })
-})
+// Read
+app.get("/", (req, res) => {
+  console.log("This is Home Route");
+  res.send({
+    marvel: "Doctor Strange",
+    Ram: "Maryaada Puroshotam",
+    data: arr,
+  });
+
+  // res.send("<h1>This is html data</h1>")
+});
+
+app.get("/superman", (req, res) => {
+  // console.log("this is superman route")
+  res.send({
+    DC: "Sperman is belong from DC",
+  });
+});
 
 // Create
-app.post('/userData',(req,res)=>{
-    console.log("Post method is working")
-    console.log(req.body)
-    const data = req.body
-    arr.push(data);
-    // res.send({success:true})
-})
+app.post("/register", async (req, res) => {
+  // console.log(req.body.gmail);
+  const { name, gmail, password } = req.body;
 
+  let user = await User.findOne({ gmail });
 
-app.listen(3000,console.log("Server is running on port 3000"))
+  if (!user){
+    user = await User.create({
+      name,
+      gmail,
+      password,
+    });
+  console.log(user);
+
+  res.json({
+    user,
+    message: "User Registered Successfully..!",
+  });}else{
+    return res.json({message:'User already Exist..'})
+  }
+
+  // arr.push(data);
+  // res.send({success:true})
+});
+
+app.listen(3000, console.log("Server is running on port 3000"));
